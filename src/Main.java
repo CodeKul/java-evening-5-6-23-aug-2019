fe04acd64ebbc6a09ce12e978c56d173bdce7db0
aniruddha <aniruddha.kudalkar@gmail.com>
2019-08-26 17:38:00 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    // state
+    int date;
+    String nm;
+
+    // behaviour
+    void currentDate() {
+        date = 45;
+    }
+
+    String whatIsName() {
+        return nm;
+    }
+}
