70eca8d273bdefd66a9b47b630b9e92c6e3c7dca
aniruddha <aniruddha.kudalkar@gmail.com>
2019-08-30 17:49:05 +0530

    constructor : types of constructor

--- src/ConstructorAndOverloading.java
+++ src/ConstructorAndOverloading.java
@@ -1,2 +1,32 @@
 public class ConstructorAndOverloading {
+
+    int col;
+    String cont;
+
+    ConstructorAndOverloading() { // default constructor
+        col = 1;
+        cont = "IN";
+    }
+
+    ConstructorAndOverloading( // parameterized
+            int col,
+            String cont
+    ) {
+        this.col = col;
+        this.cont = cont;
+    }
+
+    ConstructorAndOverloading( //copy constructor
+            ConstructorAndOverloading con
+    ) {
+        this.cont = con.cont;
+        this.col = con.col;
+    }
+
+    public static void main(String[] args) {
+         ConstructorAndOverloading co =
+                 new ConstructorAndOverloading(89,"s");
+
+         new ConstructorAndOverloading(co);
+    }
 }
