483b615af641b66af143b60f01032d9f964d15e5
aniruddha <aniruddha.kudalkar@gmail.com>
2019-08-30 17:35:00 +0530

    readme : added readme to the project

---
+++ README.md
@@ -0,0 +1,5 @@
+## Java Training In Pune, CodeKul
+
+# Style guide
+    - https://github.com/twitter/commons/blob/master/src/java/com/twitter/common/styleguide.md
+    - https://google.github.io/styleguide/javaguide.html
--- src/Blocks.java
+++ src/Blocks.java
@@ -1,15 +1,50 @@
 public class Blocks {
 
+    int objAcc = 10;
+    static int clsAcc = 50;
+
     static  {
-        System.out.println("Static");
+        clsAcc = 89;
+        System.out.println("In Static");
+
+        stFn();
     }
 
-    {
-        // init
-        System.out.println("Init");
+    /*init*/ {
+        objAcc = 78;
+        System.out.println("In Init");
+
+        nstFn();
     }
 
     Blocks() {
-        System.out.println("Constructor");
+        objAcc = 78;
+        System.out.println("In Constructor");
+
+        nstFn();
+    }
+
+    static void stFn() {
+        clsAcc = 89;
+    }
+
+    void nstFn() {
+        objAcc = 89;
+        clsAcc = 89;
+    }
+
+    public static void msadfkksajfkain(String[] aadlfkkasdlfkrgs) {
+
+        // java copyfile "./my.txt" "/desktop/hi.txt" // command line args
+
+        stFn();
+    }
+
+    public static void main(String[] args) {
+
+        //javac Calc.java
+        //java Calc 12 89
+
+        System.exit(0);
     }
 }
